796fd1a044b1c48d9b3eb45e5757d67eb5994c89
Greg Perry <chizy2@gmail.com>
2022-11-26 08:21:44 -0600

    base setup

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -3,7 +3,11 @@ import React from "react";
 class StorePicker extends React.Component {
     render() {
         return (
-            <p>I am the Store Picker!!</p>
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" />>
+                <button type="submit">Visit Store -></button>
+            </form>
         )
     }
 }
