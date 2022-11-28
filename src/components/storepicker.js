884c4fbddf320bc258639a4641b371aa1eabc46d
Greg Perry <chizy2@gmail.com>
2022-11-28 08:11:38 -0600

    fix some spelling errors

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -17,7 +17,7 @@ class StorePicker extends React.Component {
         return (
             <form className="store-selector" onSubmit={this.goTostore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" ref={this.myInpurt} required placeholder="Store Name" defaultValue={getFunName()} />>
+                <input type="text" ref={this.myInpurt} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store -></button>
             </form>
         )
