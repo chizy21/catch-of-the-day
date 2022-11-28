0368369e22ab9bbd220992ec6b84214a719fd0d4
Greg Perry <chizy2@gmail.com>
2022-11-28 17:38:44 -0600

    complete order.js

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
+    
     render() {
         const {image, name, price, description, status} = this.props.details.image;
+
+        const isAvailable = status === 'available';
 
         return <li className='menu-fish'>
             <img src={image} alt={name} />
@@ -11,7 +14,7 @@ class Fish extends React.Component {
             <span className='fish-price'>{formatPrice(price)}</span>
             </h3>
             <p>{desc}</p>
-            <button>Add to Cart</button>
+            <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>{isAvailable ? 'Add To Order' : 'Sold Out!'}</button>
         </li>;
     }
 }
